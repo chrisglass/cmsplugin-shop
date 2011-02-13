4c5532b9d7b30c29e926c40515aa95f24df2454c
Chris Glass <tribaal@gmail.com>
2011-02-13 15:52:59 +0100

    Created rendering context for the cart cms plugin Added .gitignore to source control

--- cmsplugin_shop/cms_plugins.py
+++ cmsplugin_shop/cms_plugins.py
@@ -1,5 +1,7 @@
+from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from shop.util.cart import get_or_create_cart
 
 class CartCMSPlugin(CMSPluginBase):
     model = CMSPlugin
@@ -7,6 +9,15 @@ class CartCMSPlugin(CMSPluginBase):
     render_template = "cmsplugin_shop/cart.html"
 
     def render(self, context, instance, placeholder):
+        request = context['request']
+        cart = get_or_create_cart(request)
+        cart_items = cart.items
+        
+        context.update({'cart':cart})
+        context.update({'cart_items':cart_items})
+
         return context
+
+    
 
 plugin_pool.register_plugin(CartCMSPlugin)
